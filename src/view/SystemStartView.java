bba7e9eb3a8024c763053b8978bd6e4f17a2eb56
taua herbert <tauaherbert@hotmail.com>
2026-02-23 21:16:22 -0300

    Iniciando os testes para a implementação de criptografia do Usuario (login e senha)

--- src/view/SystemStartView.java
+++ src/view/SystemStartView.java
@@ -9,17 +9,26 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
+import Utils.MessageBox;
 import daoLibrary.DBConnection;
 
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.sql.SQLException;
 import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
+
+import java.util.HexFormat;
+import java.security.MessageDigest;
+import javax.swing.JPasswordField;
+import javax.swing.JLabel;
 
 public class SystemStartView extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private String login, senha;
+	private JPasswordField jpasswordFieldSenha;
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -54,7 +63,7 @@ public class SystemStartView extends JFrame {
 		contentPane.setLayout(null);
 		
 		JButton jbtnIniciar = new JButton("Iniciar");
-		jbtnIniciar.setBounds(194, 103, 132, 71);
+		jbtnIniciar.setBounds(197, 200, 132, 71);
 		contentPane.add(jbtnIniciar);
 		
 		JButton jbtnExit = new JButton("Encerrar");
@@ -69,30 +78,62 @@ public class SystemStartView extends JFrame {
 		});
 		contentPane.add(jbtnExit);
 		
+		JTextField jtextFieldLogin = new JTextField();
+		jtextFieldLogin.setToolTipText("");
+		jtextFieldLogin.setBounds(197, 80, 132, 20);
+		contentPane.add(jtextFieldLogin);
+		jtextFieldLogin.setColumns(10);
+		
+		jpasswordFieldSenha = new JPasswordField();
+		jpasswordFieldSenha.setBounds(197, 142, 132, 20);
+		contentPane.add(jpasswordFieldSenha);
+		
+		JLabel jlblNewLabelLogin = new JLabel("Login:");
+		jlblNewLabelLogin.setBounds(197, 63, 46, 14);
+		contentPane.add(jlblNewLabelLogin);
+		
+		JLabel jlblNewLabelSenha = new JLabel("Senha:");
+		jlblNewLabelSenha.setBounds(197, 124, 46, 14);
+		contentPane.add(jlblNewLabelSenha);
+		
+		
 		jbtnIniciar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				try {
-					DBConnection.getConnection();
+				login = jtextFieldLogin.getText();
+				senha = new String (jpasswordFieldSenha.getPassword());
+				if (login.equals("system") && senha.equals("system")) {
 					
-					if (DBConnection.getConnection() != null) {
-						JOptionPane.showMessageDialog(null,"Conexão bem sucedida");
-						
-						try {
-							DBConnection.getConnection().close();
-						} catch (SQLException ex) {
-							JOptionPane.showMessageDialog(null,"Falha ao encerrar conexão: "+ex.getMessage());
+				try {
+					MessageDigest codi = MessageDigest.getInstance("SHA-256");
+					byte[] hashBytes = codi.digest(senha.getBytes());
+					
+					String protecao = HexFormat.of().formatHex(hashBytes);
+					
+					MessageBox.messageShow("Senha criptografada: "+protecao);
+					
+						if (DBConnection.getConnection() != null) {
+							JOptionPane.showMessageDialog(null,"Conexão bem sucedida");
+							
+							try {
+								DBConnection.getConnection().close();
+							} catch (SQLException ex) {
+								JOptionPane.showMessageDialog(null,"Falha ao encerrar conexão: "+ex.getMessage());
+							}
+							
+							MainView tlPrincipal = new MainView();
+							tlPrincipal.setVisible(true);
+							dispose();
+							
+						}else {
+							JOptionPane.showMessageDialog(null,"Falha ao conectar");
 						}
-						
-						MainView tlPrincipal = new MainView();
-						tlPrincipal.setVisible(true);
-						dispose();
-						
-					}else {
-						JOptionPane.showMessageDialog(null,"Falha ao conectar");
+					} catch (Exception ex) {
+						JOptionPane.showMessageDialog(null,"Erro no main. Erro: "+ex);
 					}
-				} catch (Exception ex) {
-					JOptionPane.showMessageDialog(null,"Erro no main. Erro: "+ex);
+				} else {
+					MessageBox.messageShow("Senha incorreta");
 				}
+				
 			}
 		});
 	}
